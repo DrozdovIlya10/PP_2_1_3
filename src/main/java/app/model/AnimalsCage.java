8515c177d7528717280d18629c54fa0b77628da3
DrozdovIlya10 <Drozdov.ilya4610@gmail.com>
2022-10-18 16:16:52 +0700

    2.1.3

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -12,7 +12,9 @@ public class AnimalsCage {
     @Autowired
     @Qualifier("dog")
     private Animal animal;
-    private Timer timer = new Timer();
+    @Autowired
+    private Timer timer;
+
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
